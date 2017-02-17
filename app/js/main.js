e36d944900e75e77f7bff45ef056a9ad24feeaba
andrewgremlich <andrew.gremlich@gmail.com>
2017-02-17 16:00:16 -0600

    socket connection made.

--- app/js/main.js
+++ app/js/main.js
@@ -1,242 +1,277 @@
-/**************************
-CANVAS PROTOTYPE CONSTRUCTOR
- ***************************/
+(function () {
 
-function Canvas() {
-    this.canvasElement = document.getElementById('myCanvas')
-    this.ctx = this.canvasElement.getContext('2d')
-    this.width = this.canvasElement.width
-    this.height = this.canvasElement.height
-}
+    var socket = io()
 
-/*Prototype method to draw the borders on the canvas.  Purely decorative*/
+    /**************************
+    CANVAS PROTOTYPE CONSTRUCTOR
+     ***************************/
 
-Canvas.prototype.drawBorders = function () {
-    this.ctx.beginPath()
-    this.ctx.moveTo(2, 2)
-    this.ctx.lineTo(this.width - 2, 2)
-    this.ctx.lineTo(this.width - 2, this.height - 2)
-    this.ctx.lineTo(2, this.height - 2)
-    this.ctx.lineTo(2, 2)
-    this.ctx.stroke()
-}
-
-/*
-Clear the frame for the framerate.
-*/
-
-Canvas.prototype.clearFrame = function () {
-    this.ctx.clearRect(0, 0, this.width, this.height)
-}
-
-/**************************
-ENEMY PROTOTYPE CONSTRUCTOR
-***************************/
-function Enemy(cwidth, cheight) {
-    this.width = 10
-    this.height = 10
-    this.xMotionSpeed = -4
-    this.yMotionSpeed = -2
-    this.radius = 10
-    this.wboundary = cwidth
-    this.hboundary = cheight
-    this.x = this.wboundary - 100
-    this.y = this.hboundary - 100
-    this.collisions = 0
-}
-
-/*
-Draw the enemy circle on the canvas element.
-*/
-
-Enemy.prototype.drawEnemy = function (ctx) {
-    ctx.beginPath()
-    ctx.arc(this.x, this.y, this.radius + this.collisions * 1.5, 0, Math.PI * 2)
-    ctx.fillStyle = '#a73838'
-    ctx.fill()
-    ctx.closePath()
-}
-
-/*
-Automatic motion of the enemy circle.
-*/
-
-Enemy.prototype.updateEnemyPosition = function () {
-    this.x += this.xMotionSpeed
-    this.y += this.yMotionSpeed
-}
-
-/*
-Automatic and size incrementing motion of the enemy.  Each time that the enemy
-touches a wall the enemy grows.
-*/
-
-Enemy.prototype.collision = function (cwidth, cheight) {
-
-    var xPos = this.x + this.xMotionSpeed,
-        yPos = this.y + this.yMotionSpeed,
-        collisionFactor = this.collisions * 1.5
-
-    /*The collision of an expanding circle is being detected half the time.*/
-
-    if (xPos > cwidth - this.radius - collisionFactor || xPos < this.radius + collisionFactor) {
-        this.xMotionSpeed = -this.xMotionSpeed
-        this.collisions = this.collisions + 2
-    }
-    if (yPos > cheight - this.radius - collisionFactor || yPos < this.radius + collisionFactor) {
-        this.yMotionSpeed = -this.yMotionSpeed
-        this.collisions = this.collisions + 2
+    function Canvas() {
+        this.canvasElement = document.getElementById('myCanvas')
+        this.ctx = this.canvasElement.getContext('2d')
+        this.width = this.canvasElement.width
+        this.height = this.canvasElement.height
     }
 
-}
+    /*Prototype method to draw the borders on the canvas.  Purely decorative*/
 
-/***************************
-PLAYER PROTOTYPE CONSTRUCTOR
-****************************/
+    Canvas.prototype.drawBorders = function () {
+        this.ctx.beginPath()
+        this.ctx.moveTo(2, 2)
+        this.ctx.lineTo(this.width - 2, 2)
+        this.ctx.lineTo(this.width - 2, this.height - 2)
+        this.ctx.lineTo(2, this.height - 2)
+        this.ctx.lineTo(2, 2)
+        this.ctx.stroke()
+    }
 
-function Player() {
-    this.width = 10
-    this.height = 10
-    this.x = 25
-    this.y = 25
-    this.xMotionSpeed = 0
-    this.yMotionSpeed = 0
-    this.radius = 10
-}
+    /*
+    Clear the frame for the framerate.
+    */
 
-/*
-Draw player.  It's just a circle for now.
-*/
+    Canvas.prototype.clearFrame = function () {
+        this.ctx.clearRect(0, 0, this.width, this.height)
+    }
 
-Player.prototype.drawPlayer = function (ctx) {
-    ctx.beginPath()
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
-    ctx.fillStyle = '#558bb8'
-    ctx.fill()
-    ctx.closePath()
-}
+    /**************************
+    ENEMY PROTOTYPE CONSTRUCTOR
+    ***************************/
+    function Enemy(cwidth, cheight) {
+        this.width = 10
+        this.height = 10
+        this.xMotionSpeed = -4
+        this.yMotionSpeed = -2
+        this.radius = 10
+        this.wboundary = cwidth
+        this.hboundary = cheight
+        this.x = this.wboundary - 100
+        this.y = this.hboundary - 100
+        this.collisions = 0
+    }
 
-/*
-Player motion according to the arrow controls in the movePlayer method
-*/
+    /*
+    Draw the enemy circle on the canvas element.
+    */
 
-Player.prototype.updatePlayerPosition = function () {
-    this.x += this.xMotionSpeed
-    this.y += this.yMotionSpeed
-}
+    Enemy.prototype.drawEnemy = function (ctx) {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius + this.collisions * 1.5, 0, Math.PI * 2)
+        ctx.fillStyle = '#a73838'
+        ctx.fill()
+        ctx.closePath()
+    }
 
-/*
-Evaluate collision of the player with the edges of the play area.
-*/
+    /*
+    Automatic motion of the enemy circle.
+    */
 
-Player.prototype.collision = function (cwidth, cheight) {
+    Enemy.prototype.updateEnemyPosition = function () {
+        this.x += this.xMotionSpeed
+        this.y += this.yMotionSpeed
+    }
 
-    var xPos = this.x + this.xMotionSpeed,
-        yPos = this.y + this.yMotionSpeed
+    /*
+    Automatic and size incrementing motion of the enemy.  Each time that the enemy
+    touches a wall the enemy grows.
+    */
 
-    if (xPos > cwidth - this.radius || xPos < this.radius) this.xMotionSpeed = -this.xMotionSpeed
-    if (yPos > cheight - this.radius || yPos < this.radius) this.yMotionSpeed = -this.yMotionSpeed
-}
+    Enemy.prototype.collision = function (cwidth, cheight) {
 
-/*
-Directional control with the arrow keys.
-*/
+        var xPos = this.x + this.xMotionSpeed,
+            yPos = this.y + this.yMotionSpeed,
+            collisionFactor = this.collisions * 1.5
 
-Player.prototype.movePlayer = function () {
+        /*The collision of an expanding circle is being detected half the time.*/
 
-    var that = this
+        if (xPos > cwidth - this.radius - collisionFactor || xPos < this.radius + collisionFactor) {
+            this.xMotionSpeed = -this.xMotionSpeed
+            this.collisions = this.collisions + 2
+        }
+        if (yPos > cheight - this.radius - collisionFactor || yPos < this.radius + collisionFactor) {
+            this.yMotionSpeed = -this.yMotionSpeed
+            this.collisions = this.collisions + 2
+        }
 
-    document.onkeydown = function (e) {
+    }
 
-        /*To make a static speed instead of a steady acceleration
+    /***************************
+    PLAYER PROTOTYPE CONSTRUCTOR
+    ****************************/
 
-        that.xMotionSpeed = 0
-        that.yMotionSpeed = 0
-        */
+    function Player() {
+        this.width = 10
+        this.height = 10
+        this.x = 25
+        this.y = 25
+        this.xMotionSpeed = 0
+        this.yMotionSpeed = 0
+        this.radius = 10
+    }
 
-        let keyCode = e.keyCode || e.which,
-            stringKey = keyCode.toString(),
-            move = {
-                '37': function () {
-                    /*arrow left*/
-                    that.xMotionSpeed = that.xMotionSpeed - 2
-                },
-                '38': function () {
-                    /*arrow up*/
-                    that.yMotionSpeed = that.yMotionSpeed - 2
-                },
-                '39': function () {
-                    /*arrow right*/
-                    that.xMotionSpeed = that.xMotionSpeed + 2
-                },
-                '40': function () {
-                    /*arrow down*/
-                    that.yMotionSpeed = that.yMotionSpeed + 2
+    /*
+    Draw player.  It's just a circle for now.
+    */
+
+    Player.prototype.drawPlayer = function (ctx) {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+        ctx.fillStyle = '#558bb8'
+        ctx.fill()
+        ctx.closePath()
+    }
+
+    /*
+    Player motion according to the arrow controls in the movePlayer method
+    */
+
+    Player.prototype.updatePlayerPosition = function () {
+        this.x += this.xMotionSpeed
+        this.y += this.yMotionSpeed
+    }
+
+    /*
+    Evaluate collision of the player with the edges of the play area.
+    */
+
+    Player.prototype.collision = function (cwidth, cheight) {
+
+        var xPos = this.x + this.xMotionSpeed,
+            yPos = this.y + this.yMotionSpeed
+
+        if (xPos > cwidth - this.radius || xPos < this.radius) this.xMotionSpeed = -this.xMotionSpeed
+        if (yPos > cheight - this.radius || yPos < this.radius) this.yMotionSpeed = -this.yMotionSpeed
+    }
+
+    /*
+    Directional control with the arrow keys.
+    */
+
+    Player.prototype.movePlayer = function () {
+
+        var that = this
+
+        document.onkeydown = function (e) {
+
+            /*To make a static speed instead of a steady acceleration
+
+            that.xMotionSpeed = 0
+            that.yMotionSpeed = 0
+            */
+
+            let keyCode = e.keyCode || e.which,
+                stringKey = keyCode.toString(),
+                move = {
+                    '37': function () {
+                        /*arrow left*/
+                        that.xMotionSpeed = that.xMotionSpeed - 2
+                    },
+                    '38': function () {
+                        /*arrow up*/
+                        that.yMotionSpeed = that.yMotionSpeed - 2
+                    },
+                    '39': function () {
+                        /*arrow right*/
+                        that.xMotionSpeed = that.xMotionSpeed + 2
+                    },
+                    '40': function () {
+                        /*arrow down*/
+                        that.yMotionSpeed = that.yMotionSpeed + 2
+                    }
                 }
-            }
 
-        move[stringKey]()
+            move[stringKey]()
+        }
+
+        document.onkeyup = function () {
+
+            that.xMotionSpeed = 0
+            that.yMotionSpeed = 0
+
+        }
     }
 
-    document.onkeyup = function () {
+    /*End game function.*/
+    function endgame(player, enemy) {
 
-        that.xMotionSpeed = 0
-        that.yMotionSpeed = 0
+        var enemyRadius = enemy.radius + enemy.collisions * 1.5,
+            playerRadius = player.radius,
+            middleEvalExpr = Math.pow(player.x - enemy.x, 2) + Math.pow(player.y - enemy.y, 2),
+            lowerEval = Math.pow(playerRadius - enemyRadius, 2) <= middleEvalExpr,
+            higherEval = middleEvalExpr <= Math.pow(playerRadius + enemyRadius, 2)
+
+        if (lowerEval && higherEval) {
+            setTimeout(() => {
+                clearInterval(window.inter)
+                document.querySelector('#login').style.display = 'flex'
+                document.querySelector('#canvasContainer').style.display = 'none'
+            }, 1)
+        }
 
     }
-}
 
-/**********************
-ENG GAME FUNCTION. DETECTS WHEN A PLAYER COLLIDES WITH THE ENEMY.
- **********************/
+    /*OBJECTS*/
 
-function endgame(player, enemy) {
+    document.querySelector('canvas').width = window.innerWidth
+    document.querySelector('canvas').height = window.innerHeight
 
-    var enemyRadius = enemy.radius + enemy.collisions * 1.5,
-        playerRadius = player.radius,
-        middleEvalExpr = Math.pow(player.x - enemy.x, 2) + Math.pow(player.y - enemy.y, 2),
-        lowerEval = Math.pow(playerRadius - enemyRadius, 2) <= middleEvalExpr,
-        higherEval = middleEvalExpr <= Math.pow(playerRadius + enemyRadius, 2)
+    var canvas = new Canvas(),
+        player = new Player(),
+        enemy = new Enemy(canvas.width, canvas.height)
 
-    if (lowerEval && higherEval) {
-        setTimeout(() => {
-            clearInterval(window.inter)
-        }, 1)
+    /*FRAME RATE*/
+
+    function frameRate() {
+
+        canvas.clearFrame()
+        canvas.drawBorders()
+
+        player.drawPlayer(canvas.ctx)
+        enemy.drawEnemy(canvas.ctx)
+
+        player.updatePlayerPosition()
+        enemy.updateEnemyPosition()
+
+        player.collision(canvas.width, canvas.height)
+        enemy.collision(canvas.width, canvas.height)
+
+
+        /*Pass into the function the positional data of the player and the enemy.*/
+        endgame(player, enemy)
     }
 
-}
+    /*****
+    EVENTS
+    ******/
 
-/*OBJECTS*/
+    document.querySelector('input[type="button"]').onclick = e => {
+        let srcElement = e.target || e.srcElement,
+            textInput = srcElement.previousElementSibling,
+            username = textInput.value,
+            parent = srcElement.parentElement,
+            aunt = parent.nextElementSibling
 
-document.querySelector('canvas').width = window.innerWidth
-document.querySelector('canvas').height = window.innerHeight
+        socket.emit('add user', username);
 
-var canvas = new Canvas(),
-    player = new Player(),
-    enemy = new Enemy(canvas.width, canvas.height)
+        parent.style.display = 'none'
+        aunt.style.display = 'block'
 
-/*Move player controls*/
-player.movePlayer()
+        player.movePlayer()
+        window.inter = setInterval(frameRate, 10)
+    }
 
-/*FRAME RATE*/
+    socket.on('login', e => {
+        console.log(e)
+        console.log("user connected")
+    })
 
-function frameRate() {
+    socket.on('user joined', e => {
+        console.log(e)
+        console.log("user connected")
+    })
 
-    canvas.clearFrame()
-    canvas.drawBorders()
+    socket.on('user left', e => {
+        console.log(e)
+        console.log("user left")
+    })
 
-    player.drawPlayer(canvas.ctx)
-    enemy.drawEnemy(canvas.ctx)
-
-    player.updatePlayerPosition()
-    enemy.updateEnemyPosition()
-
-    player.collision(canvas.width, canvas.height)
-    enemy.collision(canvas.width, canvas.height)
-
-
-    /*Pass into the function the positional data of the player and the enemy.*/
-    endgame(player, enemy)
-}
-
-window.inter = setInterval(frameRate, 10)
+}())
--- index.js
+++ index.js
@@ -6,8 +6,43 @@ const express = require('express'),
     port = process.env.PORT || 3000
 
 server.listen(port, function () {
-    console.log('Server listening at port %d', port);
-});
+    console.log('Server listening at port %d', port)
+})
 
 // Routing
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/app'))
+
+var numUsers = 0
+
+io.on('connection', socket => {
+    var addedUser = false
+
+
+    socket.on('add user', function (username) {
+        if (addedUser) return
+
+        socket.username = username
+        ++numUsers
+        addedUser = true
+
+        socket.emit('login', {
+            numUsers: numUsers
+        })
+
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        })
+    })
+
+    socket.on('disconnect', function () {
+        if (addedUser) {
+            --numUsers;
+
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    });
+})
